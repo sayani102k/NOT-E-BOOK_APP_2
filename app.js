6b2a2f7d479c2dea321db70fa01d3443771a8148
sayani102k <61908068+sayani102k@users.noreply.github.com>
2020-08-09 22:27:05 +0530

    second commit

--- app.js
+++ app.js
@@ -25,26 +25,18 @@ app.use(passport.session());
 mongoose.connect("mongodb://localhost:27017/ProjectWebDB", { useNewUrlParser: true, useUnifiedTopology: true});
 mongoose.set("useCreateIndex",true);
 
-const postSchema=mongoose.Schema({
-  subject:String,
-  title:String,
-  content:String
-});
-
 const userSchema=mongoose.Schema({
   email:String,
   password:String,
-  subject:String,
+  subject:String
   //userposts:postSchema
 });
 
 userSchema.plugin(passportLocalMongoose);
 
 const User=new mongoose.model("User",userSchema);
-const Post=new mongoose.model("Post",userSchema);
 
 passport.use(User.createStrategy());
-// passport.use(Post.createStrategy());
 
 //////////////SERIALISATION AND DESERIALISATION///////////////////
 
@@ -79,92 +71,8 @@ app.get("/subject",function(req,res){
 });
 
 app.get("/subject/:subjectName",function(req,res){
-
-    var subjectame=req.params.subjectName;
-    Post.find({},function(err,posts){
-    res.render(req.params.subjectName,{posts: posts});
-  });
-  app.render(req.params.subjectName);
+    res.render(req.params.subjectName);
 });
-
-
-app.get("/compose", function(req, res){
-    res.render("compose");
-    const post = new Post({
-      subject:req.body.subject,
-      title: req.body.postTitle,
-      content: req.body.postBody
-    });
-    post.save(function(err){
-      if(!err){
-        res.redirect("/compose");
-      }
-    });
-});
-
-app.post("/compose", function(req, res){
-
-});
-
-// ////////////////////////////////////////////////////////////////////////
-//
-//
-//   const subjectName=req.params.subjectName;
-//   console.log(subjectName);
-//   Post.find({},function(err,posts){
-//     res.render(subjectName,{posts: posts});
-//   });
-//   User.findById(req.user.id,function(err,foundUser){
-//     if(err){
-//       console.log(err);
-//     }else{
-//       if(foundUser){
-//         foundUser.subject=subjectName;
-//         foundUser.save(function(){
-//           //res.render("subjectName");
-//           res.render(req.params.subjectName);
-//         });
-//       }
-//     }
-//
-//   });
-//   User.find({subject:subjectName}, function(err, foundUsers){
-//   if (err){
-//     console.log(err);
-//   } else {
-//     if (foundUsers) {
-//       res.render(subjectName, {usersWithPosts: foundUsers});
-//     }
-//   }
-// });
-
-///////////////////////////////////////////////////////////////////////
-
-//{"post": {$ne: null}}
-// User.find({subject:subjectName},function(err,subjectUsers){
-//   if(subjectUsers){
-//
-// res.render(subjectName, {usersWithPosts: subjectUsers});
-//
-// }
-
-// app.get("/submit", function(req, res){
-//   if (req.isAuthenticated()){
-//     User.findOne({post:req.body.post},function(err){
-//       if(err){
-//         console.log(err);
-//       }else{
-//         res.render("submit");
-//
-//       }
-//     });
-//   } else {
-//     res.redirect("/subject");
-//   }
-// });
-// app.get("/secrets", function(req, res){
-//
-// });
 
 /////////////////////////////////POST/////////////////////////////////
 
@@ -195,13 +103,7 @@ app.post("/login", function(req, res){
       console.log(err);
     } else {
       passport.authenticate("local")(req, res, function(){
-        console.log(req.user.id);
         res.redirect("/subject");
-      //   if({subject:{$ne:null}}){
-      //   res.redirect("/subject/science");
-      // }else{
-      //   res.redirect("/subject");
-      // }
       });
     }
   });
@@ -212,25 +114,13 @@ app.post("/login", function(req, res){
 
 
 app.post("/subject",function(req,res){
-  res.render("science");
+  res.render("subject");
 });
 
 
-app.post("/subject/:subjectName",function(req,res){
-
-});
-
-app.get("/posts/:postId", function(req, res){
-
-  const requestedPostId =req.params.postId;
-
-  Post.findOne({_id: requestedPostId}, function(err, post){
-      res.render("post", {
-        title: post.title,
-        content: post.content
-      });
-  });
-});
+// app.post("/subject/:subjectName",function(req,res){
+//   res.render(req.params.subjectName)
+// });
 
 var aboutContent="Hi this is a website made with love for you!";
 var contactContent="Contact us:abcd@gmail.com"
